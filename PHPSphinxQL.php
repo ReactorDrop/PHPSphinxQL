ce611420d6e55309578ecf39f660f317dc9115dc
Andrew Seymour <commit@andrewseymour.co.uk>
2013-03-03 19:27:30 +0000

    added cleaning function for attributes, now adds in a lot of sql options and some information added

--- PHPSphinxQL.php
+++ PHPSphinxQL.php
@@ -30,7 +30,7 @@ class PHPSphinxQL
     $this->objSphinxConnection->close();
   }
 
-  public function __sphinx_connection($strQueries)
+  private function __sphinx_connection($strQueries)
   {
 
     //
@@ -58,6 +58,32 @@ class PHPSphinxQL
 
   }
 
+  /**
+   * clean up the string for the attribute name
+   *
+   * @param $strAttribute
+   * @return null|string
+   */
+  private function __clean_attribute($strAttribute)
+  {
+    $strAttribute = trim($strAttribute);
+
+    if(empty($strAttribute))
+    {
+      return null;
+    }
+    else
+    {
+      return $strAttribute;
+    }
+  }
+
+  /**
+   * connect to our sphinx server or distributed handler
+   *
+   * @param $strServer hostname or ip address (only one)
+   * @param $intPort the port at which sphinx is running on for :mysql41
+   */
   public function connect($strServer, $intPort)
   {
     // create connection
@@ -78,6 +104,11 @@ class PHPSphinxQL
 
   }
 
+  /**
+   * sets attributes that we want to select in our query
+   *
+   * @param $mixSelect an array or string of the attributes we want to select
+   */
   public function set_select($mixSelect)
   {
     // do we have an array?
@@ -103,6 +134,7 @@ class PHPSphinxQL
     }
   }
 
+
   public function set_field_weights($arrFields)
   {
     if(is_array($arrFields))
@@ -111,9 +143,23 @@ class PHPSphinxQL
     }
   }
 
+  /**
+   * filter down our results by asking for documents with particular attributes
+   *
+   * @param $strAttribute the attribute to filter by
+   * @param $mixValue the value that attribute should have
+   * @param $blnExclude are we including or excluding all documents matching this criteria
+   */
   public function set_filter($strAttribute, $mixValue, $blnExclude)
   {
+    $strAttribute = $this->__clean_attribute($strAttribute);
 
+    if(!empty($strAttribute) && !empty($mixValue) && is_string($strAttribute))
+    {
+      // add
+      $this->arrStorage['filters'][$strAttribute] = array('value' => $mixValue,
+                                                          'exclude' => $blnExclude);
+    }
   }
 
   public function set_filter_range($strAttribute, $intMin, $intMax)
@@ -121,17 +167,118 @@ class PHPSphinxQL
     
   }
 
-  public function set_group_by()
+  public function set_group_by($strAttribute, $arrSort = array())
   {
+    $strAttribute = $this->__clean_attribute($strAttribute);
 
+    if(!empty($strAttribute))
+    {
+      $this->arrStorage['group_by'] = array('attribute' => $strAttribute,
+                                            'sort' => $arrSort);
+    }
   }
 
+  /**
+   * sets options for this sql statement
+   *
+   * @param $strOption name of the option
+   * @param $mixValue the value of the option (depending on the option)
+   * @usage http://sphinxsearch.com/docs/manual-2.1.1.html#sphinxql-select
+   */
   public function set_option($strOption, $mixValue)
   {
     switch($strOption)
     {
+      case 'agent_query_timeout':
+        if(is_int($mixValue))
+        {
+          $this->arrStorage['option']['agent_query_timeout'] = $mixValue;
+        }
+
+        break;
+      case 'boolean_simplify':
+        if(is_int($mixValue) && ($mixValue === 1 || $mixValue === 0))
+        {
+          $this->arrStorage['option']['boolean_simplify'] = $mixValue;
+        }
+
+        break;
+      case 'comment':
+        if(is_string($mixValue))
+        {
+          $this->arrStorage['option']['comment'] = $mixValue;
+        }
+
+        break;
+      case 'cutoff':
+        if(is_int($mixValue))
+        {
+          $this->arrStorage['option']['cutoff'] = $mixValue;
+        }
+
+        break;
+      case 'field_weights':
+        // @TODO write this in properly later
+      case 'global_idf':
+        // @TODO write this, no documentation on this
+      case 'idf':
+        if(is_string($mixValue) && ($mixValue === 'normalized' || $mixValue === 'plain'))
+        {
+          $this->arrStorage['option']['idf'] = $mixValue;
+        }
+
+        break;
+      case 'index_weights':
+        // @TODO write this in - like field_weights
+      case 'max_matches':
+        if(is_int($mixValue))
+        {
+          $this->arrStorage['option']['max_matches'] = $mixValue;
+        }
+
+        break;
       case 'max_query_time':
-      case 'sort_mode':
+        if(is_int($mixValue))
+        {
+          $this->arrStorage['option']['max_query_time'] = $mixValue;
+        }
+
+        break;
+      case 'ranker':
+        if(is_string($mixValue) && in_array($mixValue, array('proximity_bm25', 'bm25', 'none', 'wordcount', 'proximity', 'matchany', 'fieldmask', 'sph04', 'expr', 'export')))
+        {
+          $this->arrStorage['option']['ranker'] = $mixValue;
+        }
+
+        break;
+      case 'retry_count':
+        if(is_int($mixValue))
+        {
+          $this->arrStorage['option']['retry_count'] = $mixValue;
+        }
+
+        break;
+      case 'retry_delay':
+        if(is_int($mixValue))
+        {
+          $this->arrStorage['option']['retry_delay'] = $mixValue;
+        }
+
+        break;
+      case 'reverse_scan':
+        if(is_int($mixValue) && ($mixValue === 0 || $mixValue === 1))
+        {
+          $this->arrStorage['option']['reverse_scan'] = $mixValue;
+        }
+
+        break;
+      case 'sort_method':
+        if(is_string($mixValue) && ($mixValue === 'pq' || $mixValue == 'kbuffer'))
+        {
+          $this->arrStorage['option']['sort_method'] = $mixValue;
+        }
+
+        break;
     }
   }
 
